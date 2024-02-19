551f5891fd70c9d6e76a9317a8a455c400cfc45e
Simon Christensen <31368479+simo026q@users.noreply.github.com>
2024-02-19 20:04:53 +0100

    Small improvement to label selector

--- src/DmrScraper/DmrService.cs
+++ src/DmrScraper/DmrService.cs
@@ -94,7 +94,7 @@ public class DmrService(HttpClient client)
         {
             foreach (var div in lineDivs)
             {
-                var keyNode = div.SelectSingleNode(".//div[contains(@class,'colLabel')]/label");
+                var keyNode = div.SelectSingleNode(".//div[contains(@class,'colLabel')]");
                 var valueNode = div.SelectSingleNode(".//div[contains(@class,'colValue')]/span");
 
                 if (keyNode != null && valueNode != null)
