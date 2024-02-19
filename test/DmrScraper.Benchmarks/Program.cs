237e4ced9866bf653363c16c15344d5ee6932329
Simon Christensen <31368479+simo026q@users.noreply.github.com>
2024-02-19 16:40:21 +0100

    Update benchmarks

--- test/DmrScraper.Benchmarks/Program.cs
+++ test/DmrScraper.Benchmarks/Program.cs
@@ -1,17 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 using DmrScraper;
+using HtmlAgilityPack;
 
-Console.WriteLine("Hello, World!");
+BenchmarkRunner.Run<HtmlReaderBenchmarks>();
 
 [MemoryDiagnoser(false)]
-public class Benchmarks
+public class HtmlReaderBenchmarks
 {
-    private readonly DmrService _dmrService = new(new HttpClient());
+    private static readonly HttpClient _httpClient = new();
+
+    private HttpResponseMessage _htmlResponse;
+
+    [GlobalSetup]
+    public async Task IterationSetup()
+    {
+        _htmlResponse = await _httpClient.GetAsync("https://www.google.com");
+    }
 
     [Benchmark]
-    public async Task GetDetailsAsync()
+    public async Task<HtmlDocument> LoadStream()
     {
-        await _dmrService.GetDetailsAsync("AA11111", SearchCriteria.RegistrationNumber);
+        var htmlStream = await _htmlResponse.Content.ReadAsStreamAsync();
+
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.Load(htmlStream);
+
+        return htmlDocument;
+    }
+
+    [Benchmark]
+    public async Task<HtmlDocument> LoadString()
+    {
+        var htmlString = await _htmlResponse.Content.ReadAsStringAsync();
+
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(htmlString);
+
+        return htmlDocument;
     }
 }
