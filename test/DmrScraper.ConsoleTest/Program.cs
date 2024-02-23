39f245eb7a2b9c7a2d5683a64ea8a4d8736e7968
Simon Christensen <31368479+simo026q@users.noreply.github.com>
2024-02-23 23:21:01 +0100

    Improved test console

--- test/DmrScraper.ConsoleTest/Program.cs
+++ test/DmrScraper.ConsoleTest/Program.cs
@@ -2,11 +2,35 @@
 
 var dmrService = new DmrService(new HttpClient());
 
-var details = await dmrService.GetDetailsAsync("AA11111", SearchCriteria.RegistrationNumber, includeEmpty: true);
+string registrationNumber = GeetRegistrationNumber();
+
+var details = await dmrService.GetDetailsAsync(registrationNumber, SearchCriteria.RegistrationNumber, includeEmpty: true);
 
 foreach (var (key, value) in details)
 {
     Console.WriteLine($"{key}: {value}");
 }
 
-Console.ReadLine();
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+static string GeetRegistrationNumber()
+{
+    string? registrationNumber;
+
+    do
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("? ");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("Enter registration number: ");
+        Console.ForegroundColor = ConsoleColor.Gray;
+
+        registrationNumber = Console.ReadLine();
+
+        Console.ResetColor();
+    }
+    while (string.IsNullOrWhiteSpace(registrationNumber));
+
+    return registrationNumber;
+}
