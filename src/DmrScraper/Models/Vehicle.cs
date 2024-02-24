3c66bbe4ea75401381c6c7b88d06dc45c00d294c
Simon Christensen <31368479+simo026q@users.noreply.github.com>
2024-02-24 19:32:04 +0100

    Added simple vehicle object

---
+++ src/DmrScraper/Models/Vehicle.cs
@@ -0,0 +1,9 @@
+﻿namespace DmrScraper.Models;
+
+public class Vehicle
+{
+    public string Vin { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public string Variant { get; set; }
+}
