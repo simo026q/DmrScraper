4b6d79ac38fbf24bb16630899654d840b2fb373d
Simon Christensen <31368479+simo026q@users.noreply.github.com>
2024-02-24 19:32:13 +0100

    Added vehicle parser

---
+++ src/DmrScraper/Internal/VehicleParser.cs
@@ -0,0 +1,29 @@
+﻿using DmrScraper.Models;
+
+namespace DmrScraper.Internal;
+
+internal static class VehicleParser
+{
+    public static Vehicle ParseVehicle(List<KeyValuePair<string, string>> details)
+    {
+        var vehicle = new Vehicle();
+
+        foreach (var (key, value) in details)
+        {
+            switch (key)
+            {
+                case "Stelnummer":
+                    vehicle.Vin = value;
+                    break;
+                case "Mærke, Model, Variant":
+                    var parts = value.Split(", ", 3);
+                    vehicle.Make = parts[0];
+                    vehicle.Model = parts[1];
+                    vehicle.Variant = parts[2];
+                    break;
+            }
+        }
+
+        return vehicle;
+    }
+}
