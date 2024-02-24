55cd485fc23d2b296c715bfa4de487053695f576
Simon Christensen <31368479+simo026q@users.noreply.github.com>
2024-02-24 19:42:19 +0100

    Dump vehicle object to test console as JSON

--- test/DmrScraper.ConsoleTest/Program.cs
+++ test/DmrScraper.ConsoleTest/Program.cs
@@ -1,15 +1,22 @@
 ﻿using DmrScraper;
+using System.Text.Json;
 
 var dmrService = new DmrService(new HttpClient());
 
 string registrationNumber = GeetRegistrationNumber();
 
-var details = await dmrService.GetDetailsAsync(registrationNumber, SearchCriteria.RegistrationNumber, includeEmpty: true);
+var vehicle = await dmrService.GetVehicleAsync(registrationNumber, SearchCriteria.RegistrationNumber);
+
+var vehicleJson = JsonSerializer.Serialize(vehicle, new JsonSerializerOptions { WriteIndented = true });
+
+Console.WriteLine(vehicleJson);
+
+/*var details = await dmrService.GetDetailsAsync(registrationNumber, SearchCriteria.RegistrationNumber, includeEmpty: true);
 
 foreach (var (key, value) in details)
 {
     Console.WriteLine($"{key}: {value}");
-}
+}*/
 
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
