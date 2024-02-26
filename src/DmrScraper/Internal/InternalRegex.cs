6686adb55cf9b2af578ff398769fa3774d221fc4
Simon Christensen <31368479+simo026q@users.noreply.github.com>
2024-02-26 14:24:30 +0100

    Improved DmrExecution

--- src/DmrScraper/Internal/InternalRegex.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.RegularExpressions;
-
-namespace DmrScraper.Internal;
-
-internal static partial class InternalRegex
-{
-    [GeneratedRegex("e(?<e>\\d+)s(?<s>\\d+)", RegexOptions.Compiled)]
-    public static partial Regex GetDmrExecutionRegex();
-}
